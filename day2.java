50215b5de166a26e599cdd634c9e1b6c6f95dce5
VanamaTharunKumar <130271963+VanamaTharunKumar@users.noreply.github.com>
2024-03-05 19:51:44 +0530

    Add files via upload
    Assignment -1:Accessing instance,static variables and methods in main class from a normal class.->5/3/24

---
+++ day2.java
@@ -0,0 +1,18 @@
+package one;
+//Normal used to access from main class
+public class day2 {
+//int a=5;
+//static int b=10;
+int add(int a,int b) {
+	return a+b;
+}
+static int mul(int a,int b) {
+	//System.out.println("hello");
+	int c=a*b;
+	return c;
+}
+
+
+
+}
+
---
+++ one1.java
@@ -0,0 +1,16 @@
+package one;
+
+public class one1 {
+
+	public static void main(String[] args) {
+		//Accessing instance,static variables and methods
+		day2 obj=new day2();
+		/*System.out.println(obj.a);
+		System.out.println(day2.b);*/
+		System.out.println(obj.add(5,3));
+		System.out.println(day2.mul(5,3));
+		
+		
+	}
+
+}
